f782515ae582e97b6a5555b43cf024876d4920c0
gregaid427 <gregoryd427@gmail.com>
2020-08-22 04:05:02 -1200

    Create countries.py

---
+++ UploadCovid/libs/countries.py
@@ -0,0 +1,18 @@
+countries = [
+    "Benin",
+    "Burkina Faso",
+    "Cape Verde",
+    "The Gambia",
+    "Ghana",
+    "Guinea",
+    "Guinea-Bissau",
+    "Ivory Coast",
+    "Liberia",
+    "Mali",
+    "Mauritania",
+    "Niger",
+    "Nigeria",
+    "Senegal",
+    "Sierra Leone",
+    "Togo",
+]
